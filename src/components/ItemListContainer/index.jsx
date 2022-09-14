b57f4cd46cc9dac66bd5cf8006541b8db007b252
hernan <hernanpedrozo5@gmail.com>
2022-09-13 22:17:25 -0300

    Se creo el Mock y se creo la promesa para el catalogo

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,8 +1,25 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { getProducts } from '../../asyncMock'
 
 function Itemlistcontainer({greeting}) {
+    const [products, setProducts] = useState ([])
+        useEffect(() => {
+            getProducts().then(products => {
+                setProducts(products)
+            })
+        }, [])  
+
     return (
-        <div>{greeting}</div>
+        <>
+            <h1>{greeting}</h1>
+            <ul>
+                {products.map(prod => <li key={prod.id}>{prod.name}</li> )}
+            </ul>
+        </>
+
     )
 }
+
 
 export default Itemlistcontainer
