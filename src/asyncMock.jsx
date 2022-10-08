a4be517f255dd9e5482d24d757693125bc246135
HernanPedrozo <hernanpedrozo5@gmail.com>
2022-10-08 10:51:14 -0300

    getByCategory function added

--- src/asyncMock.jsx
+++ src/asyncMock.jsx
@@ -1,14 +1,16 @@
 const products = [
     {
         "id": 1,
+        "category" : "monitor",
         "name": "Monitor Led 24 Asus Full Hd 165Hz",
         "price": 47.789,
         "image": "/img/MonitorLed24.jpeg",
         "stock": 1,
-        "destacado": "false"
+        "featured": "false"
         },  
     {
         "id": 2,
+        "category" : "placa de video",
         "name": "Placa De Video GeForce RTX 3080 Ti 12Gb Msi",
         "price": 400.999,
         "image": "/img/GeForceRTX3080.jpeg",
@@ -17,6 +19,7 @@ const products = [
         },
     {
         "id": 3,
+        "category" : "procesador",
         "name": "Procesador Amd Ryzen 7 5700G 4.6 Ghz - AM4",
         "price": 51.999,
         "image": "/img/Ryzen75700G.jpeg",
@@ -25,6 +28,7 @@ const products = [
         },
     {
         "id": 4,
+        "category" : "microfono",
         "name": "Micrófono HyperX Quadcast S Rgb",
         "price": 20.499,
         "image": "/img/microfono-hyperx.jpeg",
@@ -33,6 +37,7 @@ const products = [
         },
     {
         "id": 5,
+        "category" : "gabinete",
         "name": "Gabinete Mid Tower Thermaltake V250 rgb",
         "price": 9.599,
         "image": "/img/gabinete-midtower.jpeg",
@@ -41,6 +46,7 @@ const products = [
         },    
     {
         "id": 6,
+        "category" : "motherboard",
         "name": "Motherboard AM4 - Gigabyte GA-B450M DS3H",
         "price": 11.299,
         "image": "/img/motherboard-gigabyte.jpeg",
@@ -49,6 +55,7 @@ const products = [
         },
     {
         "id": 7,
+        "category" : "memoria",
         "name": "Memoria Ram DDR4 - 8Gb 3600 Mhz Corsair Vengeance",
         "price": 7.999,
         "image": "/img/memoria-ram.jpeg",
@@ -58,6 +65,7 @@ const products = [
         },
     {
         "id": 8,
+        "category" : "disco Solido",
         "name": "Disco Solido Ssd 480Gb Kingston A400",
         "price": 9.299,
         "image": "/img/disco-solido-ssd.jpeg",
@@ -71,6 +79,16 @@ export const getProducts = () => {
         setTimeout(() =>{
 
             resolve(products)
+            
+        }, 3000)
+    })
+}
+
+export const getByCategory = (category) => {
+    return new Promise((resolve) => {
+        setTimeout(() =>{
+            const filteredProducts = products.filter((product) => product.category === category)
+            resolve(filteredProducts)
             
         }, 3000)
     })
